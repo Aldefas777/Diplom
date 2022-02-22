8da61583ed6957f8f45c2f3f3947b10fda0c44d3
Aldefas777 <mkrvs6@gmail.com>
2022-02-22 15:44:40 +0300

    Обновление

---
+++ Controllers/CostumersController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using Project.Interfaces;
+using Project.Classes;
+
+namespace Project.Controllers
+{
+    public class CostumersController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public ICostumerRepository _costumerRepository;
+
+        public CostumersController(ILogger<HomeController> logger, ICostumerRepository costumerRepository)
+        {
+            _logger = logger;
+            _costumerRepository = costumerRepository;
+        }
+
+        public IActionResult CostumersView()
+        {
+            var model = _costumerRepository.GetUsers();
+            return View(model); ;
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            ViewBag.Id = id;
+            var model = _costumerRepository.DeleteUser(id);
+            return View(model); ;
+        }
+
+        public IActionResult Add(int Id, string names, string surname, string SecondName, string Aboniment)
+        {
+            _costumerRepository.AddUsers(Id, names, surname, SecondName, Aboniment);
+            return View();
+        }
+
+        public IActionResult Update(int? Id, string names, string surname, string SecondName, string Aboniment)
+        {
+            ViewBag.Id = Id;
+            ViewBag.Names = names;
+            _costumerRepository.UpdateUser(Id, names, surname, SecondName, Aboniment);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,38 +8,10 @@ namespace Project.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    public ICostumerRepository _costumerRepository;
-
-    public HomeController(ILogger<HomeController> logger, ICostumerRepository costumerRepository)
-    {
-        _logger = logger;
-        _costumerRepository = costumerRepository;
-    }
 
     public IActionResult Index()
     {
-        var model = _costumerRepository.GetUsers();
-        return View(model); ;
-    }
-
-    public IActionResult Delete(int? id)
-    {
-        ViewBag.Id = id;
-        var model = _costumerRepository.DeleteUser(id);
-        return View(model); ;
-    }
-
-    public IActionResult Privacy(int Id, string names, string surname, string SecondName, string Aboniment)
-    {
-        _costumerRepository.AddUsers(Id, names, surname, SecondName, Aboniment);
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(); ;
     }
 
 }
